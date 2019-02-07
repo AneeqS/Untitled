464e5112f080b110f17787124c448087a611b7e2
AneeqS <aneeqshah27@gmail.com>
2019-02-06 22:09:41 -0500

    started server

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express =  require("express");
 var app = express();
-var body_parser = require("body-parser");
+var bodyParser = require("body-parser");
+var port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.listen(port, () =>{
+   console.log("Server Started");
+});
