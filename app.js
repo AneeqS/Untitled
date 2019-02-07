4a3adbf9cd214af86bff1c203e7c1829b3292906
AneeqS <aneeqshah27@gmail.com>
2019-02-06 21:31:15 -0500

    added encoders

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 var express =  require("express");
 var app = express();
 var body_parser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
