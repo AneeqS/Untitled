19075ff9be62b16dce3a813192de8be003001f29
AneeqS <aneeqshah27@gmail.com>
2019-02-09 18:38:04 -0500

    added schema

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+//Schema
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
 var campgrounds = [
     {name: "A", image: ""},
     {name: "B", image: ""},
